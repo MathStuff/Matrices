08900444f1d39950e06d22c2c41dbd499429c182
semihM <smhmmc98@gmail.com>
2019-02-23 13:51:19 +0300

    Updated for the new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MatricesM",
-    version="0.8.alpha5",
+    version="0.9.alpha2",
     author="MathStuff members",
     author_email="business@semihmumcu.com",
 	license="GPL V3",
