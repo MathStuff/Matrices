63d7edcc0680bbd66cc14bffceac9c5df716ae3e
semihM <smhmmc98@gmail.com>
2019-04-13 18:01:09 +0300

    Added QR decomposition, small tweaks on case checkers which calls the inverse method

--- MatricesM/matrices.py
+++ MatricesM/matrices.py
@@ -904,12 +904,36 @@ EXAMPLES:
         """
         Decompose the matrix into Q and R where Q is a orthogonal matrix and R is a upper triangular matrix
         """
+        if not self.isSquare or self.isSingular:
+            return (None,None)
+        
         def _projection(vec1,vec2):
             """
-            Projection vector of vec1 over vec2
+            Projection vector of vec2 over vec1
             """
-            pass
-        pass
+            return [(sum([vec1[a]*vec2[a] for a in range(len(vec1))])/sum([a*a for a in vec1]))*c for c in vec1]
+
+        #Gram-Schmitt to get orthogonal set of the matrix
+        U=[self.col(1,0)]
+        
+        for b in range(2,self.dim[1]+1):
+            u=self.col(b,0)
+            
+            for i in range(1,b):
+                #Projection vector
+                p=_projection(U[i-1],self.col(b,0))
+                #Keep subtracting the other vectors' projections from itself
+                u=[u[i]-p[i] for i in range(len(u))]
+                
+            U.append(u.copy())
+            
+        matU = FMatrix(self.dim,U).t
+
+        #Orthonormalize by diving the columns by their norms
+        Q = matU/[sum([a*a for a in U[i]])**(1/2) for i in range(len(U))]
+        #Get the upper-triangular part
+        R = (Q.t@self).roundForm(8)
+        return (Q,R)
     
     def _hessenberg(self):
         pass
@@ -1315,13 +1339,13 @@ EXAMPLES:
     def isOrthogonal(self):
         if not self.isSquare or self.isSingular:
             return False
-        return (self.inv).roundForm(4).matrix == self.t.matrix
+        return self.inv.roundForm(4).matrix == self.t.roundForm(4).matrix
     
     @property
     def isUnitary(self):
         if not self.isSquare or self.isSingular:
             return False
-        return self.conj.t.matrix == self.inv.roundForm(4).matrix
+        return self.conj.roundForm(4).t.matrix == self.inv.roundForm(4).matrix
     
     @property
     def nilpotency(self):
@@ -1431,6 +1455,14 @@ EXAMPLES:
         if self.isSquare:
             return self._symDecomp()[1]
         return []    
+    
+    @property
+    def Q(self):
+        return self._QR()[0]
+    
+    @property
+    def R(self):
+        return self._QR()[1]    
     
     @property
     def floorForm(self):
