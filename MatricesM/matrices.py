f491b1679ffd5fbf8678ad974e6af2b77396cc98
semihM <smhmmc98@gmail.com>
2019-04-17 22:52:36 +0300

    Added nilpotency method, bug fixes

--- MatricesM/matrices.py
+++ MatricesM/matrices.py
@@ -1111,6 +1111,33 @@ EXAMPLES:
             return self._det
         else:
             return self._determinantByLUForm()  
+        
+    def nilpotency(self,limit=50):
+        """
+        Value of k for (A@A@A@...@A) == 0 where the matrix is multipled by itself k times, k in (0,inf) interval
+        limit : integer | upper bound to stop iterations
+        """
+        if not self.isSquare or self._cMat or self.isPositive:
+            return None
+        
+        from math import inf,nan
+        
+        lim = limit
+        zeroM = Matrix(self.dim,randomFill=0)
+        temp = self.copy
+        
+        for i in range(2,lim+2):
+            temp = temp@temp
+            
+            if temp.roundForm(8).matrix == zeroM.matrix:
+                return i
+            
+            lis = [temp.matrix[i][j] for i in range(temp.dim[0]) for j in range(temp.dim[1])]
+            if (inf in lis) or (nan in lis):
+                print("Some elements converged to inf")
+                return None
+           
+        return None  
     
     @property
     def diags(self):
@@ -1473,14 +1500,7 @@ EXAMPLES:
         if not self.isSquare:
             return False
         return self.isHermitian and self.isIdempotent
-    
-    @property
-    def nilpotency(self):
-        """
-        Value of k for (A@A@A@...@A) == 0 where the matrix is multipled by itself k times, k in (0,inf) interval
-        """
-        pass
-        
+       
 # =============================================================================
     """Get special formats"""
 # =============================================================================    
@@ -2532,6 +2552,7 @@ EXAMPLES:
             return self
 ################################################################################            
     def __truediv__(self,other):
+
         if isinstance(other,Matrix):
             try:
                 assert self.dim==other.dim                
