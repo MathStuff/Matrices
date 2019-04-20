d98da26f5819edb3401daaea2b36d6a28c7b63d5
semihM <smhmmc98@gmail.com>
2019-04-20 21:49:18 +0300

    Updated transpose.py

--- MatricesM/linalg/transpose.py
+++ MatricesM/linalg/transpose.py
@@ -1,18 +1,14 @@
 def transpose(mat,hermitian=False):
-    try:
-        if mat._isIdentity:
-            return mat
-        
-        temp=mat.matrix
-        d0,d1=mat.dim[0],mat.dim[1]
-        if hermitian:
-            transposed=[[temp[cols][rows].conjugate() for cols in range(d0)] for rows in range(d1)]
-        else:
-            transposed=[[temp[cols][rows] for cols in range(d0)] for rows in range(d1)]
-        
-    except Exception as err:
-        print(err)
+    if mat._isIdentity:
+        return mat
+    
+    temp=mat._matrix
+    d0,d1=mat.dim[0],mat.dim[1]
+    if hermitian:
+        transposed=[[temp[cols][rows].conjugate() for cols in range(d0)] for rows in range(d1)]
     else:
-        t = mat.copy
-        t.setMatrix((d1,d0),None,transposed)
-        return t
+        transposed=[[temp[cols][rows] for cols in range(d0)] for rows in range(d1)]
+    
+    t = mat.copy
+    t.setMatrix((d1,d0),None,transposed)
+    return t
