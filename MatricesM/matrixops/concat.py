2aafc2ad8fc5adc519e1367b1cbaefb93913feeb
semihM <smhmmc98@gmail.com>
2019-04-25 14:11:59 +0300

    dtype assertion while concatenating

--- MatricesM/matrixops/concat.py
+++ MatricesM/matrixops/concat.py
@@ -1,7 +1,7 @@
 def concat(mat,matrix,concat_as="row"):
     """
     Concatenate matrices row or columns vice
-    b:matrix to concatenate to self
+    matrix:matrix to concatenate to self
     concat_as:"row" to concat b matrix as rows, "col" to add b matrix as columns
     Note: This method concatenates the matrix to self
     """
@@ -10,21 +10,24 @@ def concat(mat,matrix,concat_as="row"):
             assert matrix.dim[1]==mat.dim[1]
         elif concat_as=="col":
             assert matrix.dim[0]==mat.dim[0]
-        b=matrix.copy
+        if matrix.dtype=="complex" and mat.dtype!="complex":
+            raise TypeError
+
     except AssertionError:
-        print("Bad dimensions at concat")
+        raise AssertionError("Dimensions don't match for concatenation")
+    except TypeError:
+        raise TypeError("Can't concatenate complex valued matrix to real valued matrix")
     else:
         if concat_as=="row":
-            for rows in b.matrix:
-                mat._matrix.append(rows)
+            for rows in range(matrix.dim[0]):
+                mat._matrix.append(matrix.matrix[rows])
 
+        elif concat_as=="col":
+            for rows in range(matrix.dim[0]):
+                mat._matrix[rows]+=matrix.matrix[rows]
         else:
-            i=0
-            for rows in mat._matrix:
-                for cols in range(b.dim[1]):
-                    rows.append(b[i,cols])
-                i+=1
-                  
+            return None    
+
         mat._Matrix__dim=mat._declareDim()
         if concat_as=="col":
             mat.features = mat.features+matrix.features
