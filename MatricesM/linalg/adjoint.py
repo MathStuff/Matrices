e955d143f84565004dc5e430282de0a010011cc4
semihM <smhmmc98@gmail.com>
2019-04-30 12:30:41 +0300

    Updated for better performance

--- MatricesM/linalg/adjoint.py
+++ MatricesM/linalg/adjoint.py
@@ -2,12 +2,6 @@ def adjoint(mat):
     if not mat.isSquare:
         return None
     else:
-        adjL=[[mat.minor(rows+1,cols+1).det*((-1)**(rows+cols)) for cols in range(mat.dim[1])] for rows in range(mat.dim[0])]
-
-        adjM = mat.floatForm
-        adjM._matrix = adjL
+        adjM=[[mat.minor(cols+1,rows+1)*((-1)**(rows+cols)) for cols in range(mat.dim[0])] for rows in range(mat.dim[0])]
         
-        mat._adj=adjM.t
-        mat._Matrix__adjCalc=1
-        
-        return mat._adj
+        return adjM
