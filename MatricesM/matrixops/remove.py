1ed7dca1aa3b0d9e64f51bed57cda6b4639dbaa9
semihM <smhmmc98@gmail.com>
2019-05-07 12:19:33 +0300

    Updated remove.py to accept both arguments given instead of one each call

--- MatricesM/matrixops/remove.py
+++ MatricesM/matrixops/remove.py
@@ -1,24 +1,17 @@
-def remove(mat,r=None,c=None):
+def remove(mat,m,n,r=None,c=None):
     """
     Deletes the given row or column
     Changes the matrix
-    Give only 1 parameter, r for row, c for column. (Starts from 1)
-    If no parameter name given, takes it as row
+    r : row number (>=1) int
+    c : column number (>=1) int
     """
-    try:
-        assert (r==None or c==None) and (r!=None or c!=None) 
-        if c==None:
-            del mat._matrix[r-1]
-            r=1
-            c=0
-        elif r==None:
-            for rows in range(mat.dim[0]):
-                del mat._matrix[rows][c-1]
-            r=0
-            c=1
-    except:
-        print("Bad arguments")
-    else:           
-        if c!=None and len(mat.features)>0:
-            del(mat.features[c-1])        
-        mat._Matrix__dim=[mat.dim[0]-r,mat.dim[1]-c]
+    if r!=None:
+        del mat._matrix[r-1]
+        m-=1
+
+    if c!=None:
+        for rows in range(m):
+            del mat._matrix[rows][c-1]
+        del mat._Matrix__features[c-1]
+        n-=1
+    mat._Matrix__dim=[m,n]
