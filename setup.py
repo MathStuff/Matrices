3b87a65fefb6ccc6ba60c704a8691c35327f0a15
semihM <smhmmc98@gmail.com>
2019-05-10 20:16:50 +0300

    Updated version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import Extension,setup,find_packages
 
-version="0.9a14.post7"
+version="0.9a14.post8"
 
 modules = [Extension("MatricesM.C_funcs.zerone",sources=["MatricesM/C_funcs/zerone.c"]),
 Extension("MatricesM.C_funcs.constructors",sources=["MatricesM/C_funcs/constructors.c"]),
