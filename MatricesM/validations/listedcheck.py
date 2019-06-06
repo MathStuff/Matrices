474caed28a4580853e6c015618d77bdcefdb1220
Semih <smhmmc98@gmail.com>
2019-06-06 22:09:47 +0300

    Delete listedcheck.py

--- MatricesM/validations/listedcheck.py
+++
@@ -1,8 +0,0 @@
-def validlist(lis,throw=False):
-    if lis == [] or lis == None:
-        if throw:
-            raise ValueError("Matrix is empty")
-        else:
-            return None
-    else:
-        return lis
