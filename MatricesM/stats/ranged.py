1fa39caa4e756c7d026a07a216198e9055516a01
semihM <smhmmc98@gmail.com>
2019-06-16 12:23:51 +0300

    Removed unnecessary attribute declaration

--- MatricesM/stats/ranged.py
+++ MatricesM/stats/ranged.py
@@ -1,13 +1,14 @@
 def ranged(mat,col=None,asDict=True):
     if isinstance(col,str):
-        col=mat.features.index(col)+1
-    mat._inRange=mat._declareRange(mat._matrix)
+        col = mat.features.index(col)+1
+    rang = mat._declareRange(mat._matrix)
+    
     if asDict:
         if col==None:
-            return mat._inRange
-        return {mat.features[col-1]:mat._inRange[mat.features[col-1]]}
+            return rang
+        return {mat.features[col-1]:rang[mat.features[col-1]]}
                 
-    items=list(mat._inRange.values())
+    items=list(rang.values())
     if len(items)==1:
         return items[0]
     
