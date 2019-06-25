5bea08378938292b47649d7450a2777bc06e7e45
semihM <smhmmc98@gmail.com>
2019-06-25 23:23:11 +0300

    Added min and max methods

---
+++ MatricesM/stats/minmax.py
@@ -0,0 +1,16 @@
+def _minmax(mat,col,dic,ismax):
+    if col==None:
+        feats = mat.features[:]
+        ranges = mat.ranged(asDict=False)
+        m = {feats[i]:ranges[i][ismax] for i in range(len(ranges))}
+    else:
+        if isinstance(col,str):
+            col = mat.features.index(col)+1
+        name = mat.features[col-1]
+        m = {name:mat.ranged(name,asDict=False)[ismax]}
+    
+    if dic:
+        return m
+    if col != None:
+        return list(m.values())[0]
+    return list(m.values())
