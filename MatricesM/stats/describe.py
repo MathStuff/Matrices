2cfff1733f4379830baa67b5d6f51ae3252f0b1a
semihM <smhmmc98@gmail.com>
2019-07-19 10:49:08 +0300

    Remove column name and add it as index

--- MatricesM/stats/describe.py
+++ MatricesM/stats/describe.py
@@ -8,10 +8,10 @@ def describe(mat,obj,dFrame):
     #Create the base of the description matrix
     valid_feats_inds = [t for t in range(len(mat.coldtypes)) if mat.coldtypes[t] in [float,int]]
     valid_feats_names = [mat.features[i] for i in valid_feats_inds]
-
-    desc_mat = obj((len(valid_feats_inds),10),fill=0,
-                  features=["Column","dtype","count","mean","sdev","min","25%","50%","75%","max"],
-                  dtype=dFrame,coldtypes=[str,type,int,float,float,float,float,float,float,float])
+    desc_mat = obj((len(valid_feats_inds),9),fill=0,
+                  features=["dtype","count","mean","sdev","min","25%","50%","75%","max"],
+                  dtype=dFrame,coldtypes=[type,int,float,float,float,float,float,float,float],
+                  index=valid_feats_names)
 
     #Gather the data
     dtypes = [mat.coldtypes[t] for t in valid_feats_inds]
@@ -25,7 +25,7 @@ def describe(mat,obj,dFrame):
     temp = []
     for i in range(len(valid_feats_inds)):
         name = valid_feats_names[i]
-        temp.append([name,dtypes[i],counts[name],mean[name],sdev[name],ranges[name][0],iqrs[name][0],iqrs[name][1],iqrs[name][2],ranges[name][1]])
+        temp.append([dtypes[i],counts[name],mean[name],sdev[name],ranges[name][0],iqrs[name][0],iqrs[name][1],iqrs[name][2],ranges[name][1]])
 
     desc_mat._matrix = temp
     desc_mat.decimal = mat.PRECISION
@@ -33,5 +33,4 @@ def describe(mat,obj,dFrame):
     unused_names = ",".join([name for name in mat.features if not name in valid_feats_names])
     unused = f"{mat.d1-len(valid_feats_names)} Unused columns : \n\t{unused_names}" if len(unused_names) else "" 
     desc_mat.NOTES = f"Size: {mat.dim}\n"+unused
-    
     return desc_mat
