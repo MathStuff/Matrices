fd4c6166f70c20ed8323f4e4d6d0912b6bd99391
semihM <smhmmc98@gmail.com>
2019-07-19 10:35:29 +0300

    Add 'rowind' option

--- MatricesM/filter/find.py
+++ MatricesM/filter/find.py
@@ -1,20 +1,21 @@
-def find(mat,dims,element,start=1):
+def find(mat,dims,element,start,rowind):
     class empty:
-        def __init__(self):
-            self.empty=True
+        pass
         
-    indeces=[]
+    indices=[]
     try:
         assert start==0 or start==1
         assert isinstance(element,int) or isinstance(element,float) or isinstance(element,complex) or isinstance(element,str)
         for row in range(dims[0]):
             while element in mat[row]:
                 n=mat[row].index(element)
-                indeces.append((row+start,n+start))
+                indices.append((row+start,n+start))
                 mat[row][n]=empty
     except AssertionError:
         print("Invalid arguments")
     else:
-        if len(indeces):
-            return indeces
+        if len(indices):
+            if rowind:
+                return list(set([i[0] for i in indices]))
+            return indices
         return None
