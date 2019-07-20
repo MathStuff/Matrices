b06643863da93fabb8e0dac63bb16bd30ce06f90
semihM <smhmmc98@gmail.com>
2019-07-20 13:15:54 +0300

    Remove unnecessary handler

--- MatricesM/filter/find.py
+++ MatricesM/filter/find.py
@@ -1,21 +1,18 @@
 def find(mat,dims,element,start,rowind):
+    assert isinstance(start,int), "Starting index have to be an integer"
+    
     class empty:
         pass
-        
+
     indices=[]
-    try:
-        assert start==0 or start==1
-        assert isinstance(element,int) or isinstance(element,float) or isinstance(element,complex) or isinstance(element,str)
-        for row in range(dims[0]):
-            while element in mat[row]:
-                n=mat[row].index(element)
-                indices.append((row+start,n+start))
-                mat[row][n]=empty
-    except AssertionError:
-        print("Invalid arguments")
-    else:
-        if len(indices):
-            if rowind:
-                return list(set([i[0] for i in indices]))
-            return indices
-        return None
+    for row in range(dims[0]):
+        while element in mat[row]:
+            n=mat[row].index(element)
+            indices.append((row+start,n+start))
+            mat[row][n]=empty
+
+    if len(indices):
+        if rowind:
+            return list(set([i[0] for i in indices]))
+        return indices
+    return None
