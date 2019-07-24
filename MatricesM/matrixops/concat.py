6cc6d5dca2e57afa15e25c2c705b62936c18a822
semihM <smhmmc98@gmail.com>
2019-07-24 12:43:01 +0300

    Update parameter name

--- MatricesM/matrixops/concat.py
+++ MatricesM/matrixops/concat.py
@@ -1,14 +1,8 @@
-def concat(mat,matrix,concat_as="col"):
-    """
-    Concatenate matrices row or columns vice
-    matrix:matrix to concatenate to self
-    concat_as:"row" to concat b matrix as rows, "col" to add b matrix as columns
-    Note: This method concatenates the matrix to self
-    """
+def concat(mat,matrix,axis):
     try:
-        if concat_as=="row":
+        if axis==0:
             assert matrix.dim[1]==mat.dim[1]
-        elif concat_as=="col":
+        elif axis==1:
             assert matrix.dim[0]==mat.dim[0]
         if matrix.dtype==complex and mat.dtype!=complex:
             raise TypeError
@@ -18,18 +12,18 @@ def concat(mat,matrix,concat_as="col"):
     except TypeError:
         raise TypeError("Can't concatenate complex valued matrix to real valued matrix")
     else:
-        if concat_as=="row":
+        if axis==0:
             for rows in range(matrix.dim[0]):
                 mat._matrix.append(matrix.matrix[rows])
 
-        elif concat_as=="col":
+        elif axis==1:
             for rows in range(matrix.dim[0]):
                 mat._matrix[rows]+=matrix.matrix[rows]
         else:
             return None    
 
         mat._Matrix__dim=mat._declareDim()
-        if concat_as=="col":
+        if axis==1:
             mat._Matrix__features = mat.features + [i if i not in mat.features else "_"+i for i in matrix.features]
             mat._Matrix__coldtypes = mat.coldtypes + [i for i in matrix.coldtypes]
         else:
