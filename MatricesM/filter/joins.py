985bcd5d56253a2cce22bd7d157a1a1fbc4e01c4
semihM <smhmmc98@gmail.com>
2019-07-25 16:49:22 +0300

    Move 'join' method to 'matrixops' directory

--- MatricesM/filter/joins.py
+++
@@ -1,7 +0,0 @@
-def joins(mat,table,method,obj):
-    if not method in ["inner","left","left-ex","right","right-ex","outer","outer-ex"]:
-        raise ValueError(f"{method} is not a join method.")
-    if not isinstance(table,obj):
-        raise TypeError(f"Can't use type {type(table).__name__} as a Matrix")
-    
-    
