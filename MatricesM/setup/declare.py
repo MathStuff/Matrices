75bf4dc880ebcf075af378d10b4f9e1b42aac18b
semihM <smhmmc98@gmail.com>
2019-07-25 23:19:33 +0300

    Updated range declaration to work better with invalid data

--- MatricesM/setup/declare.py
+++ MatricesM/setup/declare.py
@@ -1,7 +1,4 @@
 def declareDim(mat):
-    """
-    Set new dimension 
-    """
     m = mat._matrix
     if m == None:
         mat._matrix = []
@@ -15,25 +12,42 @@ def declareDim(mat):
     return [rows,cols]
     
 def declareRange(mat,lis):
-    """
-    Finds and returns the range of the elements in a given list
-    """
     c={}
+    #Dataframe
     if mat._dfMat:
-        valid_feats_inds = [t for t in range(len(mat.coldtypes)) if mat.coldtypes[t] in [float,int]]
+        valid_feats_inds = [t for t in range(mat.d1) if mat.coldtypes[t] in [float,int]]
         for cols in valid_feats_inds:
             current = mat.col(cols+1,0)
             temp=[val for val in current if isinstance(val,(int,float))]
-            c[mat.features[cols]]=[min(temp),max(temp)]
+            try:
+                mn,mx = min(temp),max(temp)
+            except:
+                #All values are invalid
+                temp=[len(str(val)) for val in current ]
+                c[mat.features[cols]]=[min(temp),max(temp)]
+            else:
+                c[mat.features[cols]]=[mn,mx]
+
+    #Complex dtype
     elif mat._cMat:
-        for i in range(mat.dim[1]):
-            temp=[]
-            for rows in range(mat.dim[0]):
-                temp.append(lis[rows][i].real)
-                temp.append(lis[rows][i].imag)
-            c[mat.features[i]]=[min(temp),max(temp)]
+        try:
+            for i in range(mat.dim[1]):
+                temp=[]
+                for rows in range(mat.dim[0]):
+                    temp.append(lis[rows][i].real)
+                    temp.append(lis[rows][i].imag)
+                c[mat.features[i]]=[min(temp),max(temp)]
+        except AttributeError:
+            raise TypeError(f"complex dtype matrix only allows complex numbers as its elements")
+
+    #Float or int dtype
     else:
-        for cols in range(mat.dim[1]):
-            temp=mat.col(cols+1,0)
-            c[mat.features[cols]]=[min(temp),max(temp)]
+        try:
+            for cols in range(mat.dim[1]):
+                temp=mat.col(cols+1,0)
+                c[mat.features[cols]]=[min(temp),max(temp)]
+        except (TypeError,ValueError):
+            typ = [int,float][mat._fMat]
+            raise ValueError(f"{typ} dtype matrix can't have non-{typ} values")
+
     return c
