35e91cde574bec6c94ea500771db292b46748a84
semihM <smhmmc98@gmail.com>
2019-07-28 18:25:19 +0300

    Change 'empty' to 'null' class

--- MatricesM/filter/find.py
+++ MatricesM/filter/find.py
@@ -1,15 +1,14 @@
 def find(mat,dims,element,start,rowind):
     assert isinstance(start,int), "Starting index have to be an integer"
     
-    class empty:
-        pass
-
+    from MatricesM.customs.objects import null
+    
     indices=[]
     for row in range(dims[0]):
         while element in mat[row]:
             n=mat[row].index(element)
             indices.append((row+start,n+start))
-            mat[row][n]=empty
+            mat[row][n]=null
 
     if len(indices):
         if rowind:
