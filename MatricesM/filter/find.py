b7c9b3752d1a4de1a7bfa2d95e12f017b3cf690a
semihM <smhmmc98@gmail.com>
2019-07-30 17:40:51 +0300

    Replace 'null' class with temporary 'empty' class

--- MatricesM/filter/find.py
+++ MatricesM/filter/find.py
@@ -1,14 +1,15 @@
 def find(mat,dims,element,start,rowind):
     assert isinstance(start,int), "Starting index have to be an integer"
     
-    from MatricesM.customs.objects import null
+    class empty:
+        pass
     
     indices=[]
     for row in range(dims[0]):
         while element in mat[row]:
             n=mat[row].index(element)
             indices.append((row+start,n+start))
-            mat[row][n]=null
+            mat[row][n]=empty
 
     if len(indices):
         if rowind:
