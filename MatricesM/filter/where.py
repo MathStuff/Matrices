c034e2b04e8428ebbfe8f97a4af8f1882c01a65c
semihM <smhmmc98@gmail.com>
2019-08-01 13:37:47 +0300

    Add 'inplace' option for dev

--- MatricesM/filter/where.py
+++ MatricesM/filter/where.py
@@ -1,22 +1,23 @@
-def wheres(mat,conds,feats):
+def wheres(mat,conds,feats,inplace):
+    if inplace:
+        #Replace comparison operators with proper symbols
+        if " and " in conds:
+            conds = conds.replace(" and ","&")
+        if " or " in conds:
+            conds = conds.replace(" or ","|")
 
-    #Replace comparison operators with proper symbols
-    if " and " in conds:
-        conds = conds.replace(" and ","&")
-    if " or " in conds:
-        conds = conds.replace(" or ","|")
+        #Replace feature names with column matrices
+        for f in feats:
+            if f in conds:
+                conds = conds.replace(f,f"mat.col({feats.index(f)+1})")
 
-    #Replace feature names with column matrices
-    for f in feats:
-        if f in conds:
-            conds = conds.replace(f,f"mat.col({feats.index(f)+1})")
-
-    #Apply the conditions and find out where it is True
-    allinds = eval(conds).find(1,0)
-    if allinds == None:
-        raise ValueError("No data found")
-        
-    inds = [i[0] for i in eval(conds).find(1,0)]
-    filtered = [mat.matrix[i][:] for i in inds]
-    return (filtered,inds)
-
+        #Apply the conditions and find out where it is True
+        allinds = eval(conds).find(1,0)
+        if allinds == None:
+            raise ValueError("No data found")
+            
+        inds = [i[0] for i in eval(conds).find(1,0)]
+        filtered = [mat.matrix[i][:] for i in inds]
+        return (filtered,inds)
+    else:
+        pass
