190e9f532a4d8fae68d71722e3573f50782839fd
semihM <smhmmc98@gmail.com>
2019-11-11 21:35:45 +0300

    Update transposing operation for multi-leveled row labels and column names

--- MatricesM/linalg/transpose.py
+++ MatricesM/linalg/transpose.py
@@ -1,15 +1,18 @@
-def transpose(mat,hermitian=False,obj=None):
+def transpose(mat,hermitian=False,obj=None,labelobj=None):
+    from MatricesM.customs.objects import Label
+    
     if mat.isIdentity:
         return mat
     
     temp=mat._matrix
-    d0,d1=mat.dim[0],mat.dim[1]
+    d0,d1=mat.dim
     if hermitian:
         transposed=[[temp[cols][rows].conjugate() for cols in range(d0)] for rows in range(d1)]
     else:
         from MatricesM.C_funcs.linalg import Ctranspose
         transposed = Ctranspose(d0,d1,temp)
 
-    old_f = mat.features[:] if mat._dfMat else None
-    old_i = [str(label_tuple[0]) for label_tuple in mat.index.labels] if mat._dfMat else []
-    return obj((d1,d0),transposed,dtype=mat.dtype,features=old_i,index=old_f,implicit=True)
+    inds = mat.features[:] if mat._dfMat else None
+    feats = Label([tuple([str(label) for label in row]) for row in mat.index.labels[:]],mat.index.names) if mat._dfMat else None
+
+    return obj((d1,d0),transposed,dtype=mat.dtype,features=feats,index=inds,implicit=True)
