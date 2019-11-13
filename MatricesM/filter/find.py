2a5aea029adf84eb4522426e8295db3efe9bdc3f
semihM <smhmmc98@gmail.com>
2019-11-13 19:26:55 +0300

    Update 'find' method for better performance Add coldtype guesser to 'apply' method Update 'apply' method to work with multilevel column names

--- MatricesM/filter/find.py
+++ MatricesM/filter/find.py
@@ -1,18 +1,16 @@
 def find(mat,dims,element,start,rowind):
     assert isinstance(start,int), "Starting index have to be an integer"
     
-    class empty:
-        pass
-    
     indices=[]
-    for row in range(dims[0]):
-        while element in mat[row]:
-            n=mat[row].index(element)
-            indices.append((row+start,n+start))
-            mat[row][n]=empty
+    d0,d1 = dims
+
+    for i in range(d0):
+        mm = mat[i]
+        for j in range(d1):
+            if mm[j] == element:
+                indices.append((i+start,j+start))
 
     if len(indices):
         if rowind:
             return list(set([i[0] for i in indices]))
         return indices
-    return None
