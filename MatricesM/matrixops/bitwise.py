da89ff580c3a821a117185ddc189c8b4cde05b1f
semihM <smhmmc98@gmail.com>
2019-12-09 13:08:29 +0300

    Re-write comparison operations, Enable better indexing with boolean matrices, Minor fixes and performance tweaks

--- MatricesM/matrixops/bitwise.py
+++ MatricesM/matrixops/bitwise.py
@@ -9,101 +9,205 @@ def _invert(mat,intform):
 def _and(mat,other,obj,m):
     """
     Can only be used with '&' operator not with 'and'
+
+    Multi-column boolean matrices' values are compared with 'and' operator, meaning that 1 false value
+    causes whole row to be reduced to a false value
     """
-    try:
+    if mat.BOOL_MAT:
         if isinstance(other,obj):
             if mat.dim!=other.dim:
                 raise ValueError("Dimensions of the matrices don't match")
+            if not other.BOOL_MAT:
+                raise TypeError("Can't compare bool matrix to non-bool matrix")
+            
+            d0,d1 = mat.dim
             o = other.matrix
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) and bool(o[j][i])) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
+            true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+            data = []
+            
+            #Reduce multiple columns into one
+            #Remove rows with false boolean values
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                if (false in mrow) or (false in orow):
+                    data.append([false])
+                    continue
+                data.append([true])
+
+            return obj(dim=[d0,1],
+                       data=data,
+                       features=mat.features[:1],
+                       index=mat.index[:],
+                       implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
+
+    else:
+        d0,d1 = mat.dim
+        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        data = []
+
+        if isinstance(other,obj):
+            if mat.dim!=other.dim:
+                raise ValueError("Dimensions of the matrices don't match")
+            if other.BOOL_MAT:
+                raise TypeError("Can't compare non-bool matrix to bool matrix")
+
+            o = other.matrix
+            
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if (bool(mrow[j]) and bool(orow[j])) else false for j in range(d1)])
         
         elif isinstance(other,list):
-            if mat.dim[1]!=len(other):
+            if mat.d1!=len(other):
                 raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) and bool(other[i])) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
-            if mat._cMat:
-                raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) and bool(other)) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
-        
-        elif isinstance(other,complex):
-            if not mat._cMat:
-                raise TypeError("Can't compare complex numbers to int/float")
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) and bool(other)) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
+
+            for i in range(d0):
+                mrow = m[i]
+                data.append([true if (bool(mrow[j]) and bool(other[j])) else false for j in range(d1)])
+
         else:
-            raise TypeError("Invalid type to compare")
+            for i in range(d0):
+                mrow = m[i]
+                data.append([true if (bool(mrow[j]) and bool(other)) else false for j in range(d1)])
             
-    except Exception as err:
-        raise err
-        
-    else:
-        return temp
+        return obj(dim=[d0,d1],
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
 
 def _or(mat,other,obj,m):
     """
     Can only be used with '|' operator not with 'or'
     """
-    try:
+    if mat.BOOL_MAT:
         if isinstance(other,obj):
             if mat.dim!=other.dim:
                 raise ValueError("Dimensions of the matrices don't match")
+            if not other.BOOL_MAT:
+                raise TypeError("Can't compare bool matrix to non-bool matrix")
+            
+            d0,d1 = mat.dim
             o = other.matrix
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) or bool(o[j][i])) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
+            true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+            data = []
+            
+            #Reduce multiple columns into one
+            #Remove rows with false boolean values
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                if (false in mrow) and (false in orow):
+                    data.append([false])
+                    continue
+                data.append([true])
+
+            return obj(dim=[d0,1],
+                       data=data,
+                       features=mat.features[:1],
+                       index=mat.index[:],
+                       implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
+
+    else:
+        d0,d1 = mat.dim
+        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        data = []
+
+        if isinstance(other,obj):
+            if mat.dim!=other.dim:
+                raise ValueError("Dimensions of the matrices don't match")
+            if other.BOOL_MAT:
+                raise TypeError("Can't compare non-bool matrix to bool matrix")
+
+            o = other.matrix
+            
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if (bool(mrow[j]) or bool(orow[j])) else false for j in range(d1)])
         
         elif isinstance(other,list):
-            if mat.dim[1]!=len(other):
+            if mat.d1!=len(other):
                 raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) or bool(other[i])) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
-            if mat._cMat:
-                raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) or bool(other)) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
-        
-        elif isinstance(other,complex):
-            if not mat._cMat:
-                raise TypeError("Can't compare complex numbers to int/float")
-            temp=obj(mat.dim,[[1 if (bool(m[j][i]) or bool(other)) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
+
+            for i in range(d0):
+                mrow = m[i]
+                data.append([true if (bool(mrow[j]) or bool(other[j])) else false for j in range(d1)])
+
         else:
-            raise TypeError("Invalid type to compare")
+            for i in range(d0):
+                mrow = m[i]
+                data.append([true if (bool(mrow[j]) or bool(other)) else false for j in range(d1)])
             
-    except Exception as err:
-        raise err
-        
-    else:
-        return temp
+        return obj(dim=[d0,d1],
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
     
 def _xor(mat,other,obj,m):
     """
     Can only be used with '^' operator 
     """
-    try:
+    if mat.BOOL_MAT:
         if isinstance(other,obj):
             if mat.dim!=other.dim:
                 raise ValueError("Dimensions of the matrices don't match")
+            if not other.BOOL_MAT:
+                raise TypeError("Can't compare bool matrix to non-bool matrix")
+            
+            d0,d1 = mat.dim
             o = other.matrix
-            temp=obj(mat.dim,[[1 if bool(m[j][i])!=bool(o[j][i]) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
+            true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+            data = []
+            
+            #Reduce multiple columns into one
+            #Remove rows with false boolean values
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                false_mrow = false in mrow
+                false_orow = false in orow
+                if false_mrow == false_orow:
+                    data.append([false])
+                    continue
+                data.append([true])
+
+            return obj(dim=[d0,1],
+                       data=data,
+                       features=mat.features[:1],
+                       index=mat.index[:],
+                       implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
+
+    else:
+        d0,d1 = mat.dim
+        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        data = []
+
+        if isinstance(other,obj):
+            if mat.dim!=other.dim:
+                raise ValueError("Dimensions of the matrices don't match")
+            if other.BOOL_MAT:
+                raise TypeError("Can't compare non-bool matrix to bool matrix")
+
+            o = other.matrix
+            
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if (bool(mrow[j]) != bool(orow[j])) else false for j in range(d1)])
         
         elif isinstance(other,list):
-            if mat.dim[1]!=len(other):
+            if mat.d1!=len(other):
                 raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(mat.dim,[[1 if bool(m[j][i])!=bool(other[i]) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
-            if mat._cMat:
-                raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(mat.dim,[[1 if bool(m[j][i])!=bool(other) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
-        
-        elif isinstance(other,complex):
-            if not mat._cMat:
-                raise TypeError("Can't compare complex numbers to int/float")
-            temp=obj(mat.dim,[[1 if bool(m[j][i])!=bool(other) else 0 for i in range(mat.dim[1])] for j in range(mat.dim[0])],dtype=int,implicit=True)
+
+            for i in range(d0):
+                mrow = m[i]
+                data.append([true if (bool(mrow[j]) != bool(other[j])) else false for j in range(d1)])
+
         else:
-            raise TypeError("Invalid type to compare")
+            for i in range(d0):
+                mrow = m[i]
+                data.append([true if (bool(mrow[j]) != bool(other)) else false for j in range(d1)])
             
-    except Exception as err:
-        raise err
-        
-    else:
-        return temp
+        return obj(dim=[d0,d1],
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
--- MatricesM/matrixops/comparison.py
+++ MatricesM/matrixops/comparison.py
@@ -1,6 +1,6 @@
-def le(mat,other,obj,m,from_wheres,matrixobj):
+def eq(mat,other,obj,m,from_wheres,matrixobj):
     if not from_wheres:
-        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        true ,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
         d0,d1 = mat.dim
         if mat._cMat:
             raise TypeError("Can't compare complex numbers")
@@ -13,28 +13,130 @@ def le(mat,other,obj,m,from_wheres,matrixobj):
                 raise ValueError("Dimensions of the matrices don't match")
 
             o = other.matrix
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<=o[j][i] else false for i in range(d1)] for j in range(d0)],implicit=True)
+            data = []
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if mrow[j]==orow[j] else false for j in range(d1)])
         
         elif isinstance(other,list):
             if d1!=len(other):
                 raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<=other[i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
-            if mat._cMat:
-                raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<=other else false for i in range(d1)] for j in range(d0)],implicit=True)
-                    
-        elif isinstance(other,str):
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<=other else false for i in range(d1)] for j in range(d0)],implicit=True)
-          
-        else:
-            raise TypeError("Invalid type to compare")
 
-        return temp
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]==other[j] else false for j in range(d1)])
+
+        else:
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]==other else false for j in range(d1)])
+            
+        
+        return obj(dim=mat.dim,
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
 
     else:
-       pass
+        pass
+    
+def ne(mat,other,obj,m,from_wheres,matrixobj):
+    if not from_wheres:
+        true ,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        d0,d1 = mat.dim
+        if mat._cMat:
+            raise TypeError("Can't compare complex numbers")
+
+        if isinstance(other,(obj,matrixobj)):
+            if other._cMat:
+                raise TypeError("Can't compare complex numbers")
+
+            if mat.dim!=other.dim:
+                raise ValueError("Dimensions of the matrices don't match")
+
+            o = other.matrix
+            data = []
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if mrow[j]!=orow[j] else false for j in range(d1)])
+        
+        elif isinstance(other,list):
+            if d1!=len(other):
+                raise ValueError("Length of the list doesn't match matrix's column amount")
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]!=other[j] else false for j in range(d1)])
+
+        else:
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]!=other else false for j in range(d1)])
+            
+        
+        return obj(dim=mat.dim,
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
+
+    else:
+        pass
+
+def le(mat,other,obj,m,from_wheres,matrixobj):
+    if not from_wheres:
+        true ,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        d0,d1 = mat.dim
+        if mat._cMat:
+            raise TypeError("Can't compare complex numbers")
+
+        if isinstance(other,(obj,matrixobj)):
+            if other._cMat:
+                raise TypeError("Can't compare complex numbers")
+
+            if mat.dim!=other.dim:
+                raise ValueError("Dimensions of the matrices don't match")
+
+            o = other.matrix
+            data = []
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if mrow[j]<=orow[j] else false for j in range(d1)])
+        
+        elif isinstance(other,list):
+            if d1!=len(other):
+                raise ValueError("Length of the list doesn't match matrix's column amount")
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]<=other[j] else false for j in range(d1)])
+
+        elif isinstance(other,(int,float,str)):
+            if mat._cMat:
+                raise TypeError("Can't compare int/float to complex numbers")
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]<=other else false for j in range(d1)])
+            
+        else:
+            raise TypeError("Invalid type to compare")
+        
+        return obj(dim=mat.dim,
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
+
+    else:
+        pass
     
 def lt(mat,other,obj,m,from_wheres,matrixobj):
     if not from_wheres:
@@ -51,110 +153,44 @@ def lt(mat,other,obj,m,from_wheres,matrixobj):
                 raise ValueError("Dimensions of the matrices don't match")
 
             o = other.matrix
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<o[j][i] else false for i in range(d1)] for j in range(d0)],implicit=True)
+            data = []
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if mrow[j]<orow[j] else false for j in range(d1)])
         
         elif isinstance(other,list):
             if d1!=len(other):
                 raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<other[i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]<other[j] else false for j in range(d1)])
+
+        elif isinstance(other,(int,float,str)):
             if mat._cMat:
                 raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<other else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,str):
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]<other else false for i in range(d1)] for j in range(d0)],implicit=True)
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]<other else false for j in range(d1)])
             
         else:
             raise TypeError("Invalid type to compare")
         
-        return temp
+        return obj(dim=mat.dim,
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
 
-    else:
-        pass
-    
-def eq(mat,other,obj,m,from_wheres,matrixobj):
-    if not from_wheres:
-        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
-        d0,d1 = mat.dim
-        if isinstance(other,(obj,matrixobj)):
-            if mat.dim!=other.dim:
-                raise ValueError("Dimensions of the matrices don't match")
-            o = other.matrix
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]==o[j][i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,list):
-            if d1!=len(other):
-                raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]==other[i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
-            if mat._cMat:
-                raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]==other else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,complex):
-            if not mat._cMat:
-                raise TypeError("Can't compare complex numbers to int/float")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]==other else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,str):
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]==other else false for i in range(d1)] for j in range(d0)],implicit=True)
-
-        elif other is None:
-            return False
-          
-        else:
-            raise TypeError("Invalid type to compare")
-
-        return temp
-
-    else:
-        pass
-    
-def ne(mat,other,obj,m,from_wheres,matrixobj):
-    if not from_wheres:
-        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
-        d0,d1 = mat.dim
-        if isinstance(other,(obj,matrixobj)):
-            if mat.dim!=other.dim:
-                raise ValueError("Dimensions of the matrices don't match")
-            o = other.matrix
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]!=o[j][i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,list):
-            if d1!=len(other):
-                raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]!=other[i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
-            if mat._cMat:
-                raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]!=other else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,complex):
-            if not mat._cMat:
-                raise TypeError("Can't compare complex numbers to int/float")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]!=other else false for i in range(d1)] for j in range(d0)],implicit=True)
-                    
-        elif isinstance(other,str):
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]!=other else false for i in range(d1)] for j in range(d0)],implicit=True)
- 
-        elif other is None:
-            return True
-                     
-        else:
-            raise TypeError("Invalid type to compare")
-
-        return temp
-    
     else:
         pass
             
 def ge(mat,other,obj,m,from_wheres,matrixobj):
     if not from_wheres:
-        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        true ,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
         d0,d1 = mat.dim
         if mat._cMat:
             raise TypeError("Can't compare complex numbers")
@@ -167,32 +203,44 @@ def ge(mat,other,obj,m,from_wheres,matrixobj):
                 raise ValueError("Dimensions of the matrices don't match")
 
             o = other.matrix
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>=o[j][i] else false for i in range(d1)] for j in range(d0)],implicit=True)
+            data = []
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if mrow[j]>=orow[j] else false for j in range(d1)])
         
         elif isinstance(other,list):
             if d1!=len(other):
                 raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>=other[i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]>=other[j] else false for j in range(d1)])
+
+        elif isinstance(other,(int,float,str)):
             if mat._cMat:
                 raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>=other else false for i in range(d1)] for j in range(d0)],implicit=True)
-                    
-        elif isinstance(other,str):
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>=other else false for i in range(d1)] for j in range(d0)],implicit=True)
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]>=other else false for j in range(d1)])
             
         else:
             raise TypeError("Invalid type to compare")
+        
+        return obj(dim=mat.dim,
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
 
-        return temp
-    
     else:
         pass
 
 def gt(mat,other,obj,m,from_wheres,matrixobj):
     if not from_wheres:
-        true,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
+        true ,false = mat.DEFAULT_BOOL[True],mat.DEFAULT_BOOL[False]
         d0,d1 = mat.dim
         if mat._cMat:
             raise TypeError("Can't compare complex numbers")
@@ -205,25 +253,37 @@ def gt(mat,other,obj,m,from_wheres,matrixobj):
                 raise ValueError("Dimensions of the matrices don't match")
 
             o = other.matrix
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>o[j][i] else false for i in range(d1)] for j in range(d0)],implicit=True)
+            data = []
+            for i in range(d0):
+                mrow,orow = m[i],o[i]
+                data.append([true if mrow[j]>orow[j] else false for j in range(d1)])
         
         elif isinstance(other,list):
             if d1!=len(other):
                 raise ValueError("Length of the list doesn't match matrix's column amount")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>other[i] else false for i in range(d1)] for j in range(d0)],implicit=True)
-        
-        elif isinstance(other,int) or isinstance(other,float):
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]>other[j] else false for j in range(d1)])
+
+        elif isinstance(other,(int,float,str)):
             if mat._cMat:
                 raise TypeError("Can't compare int/float to complex numbers")
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>other else false for i in range(d1)] for j in range(d0)],implicit=True)
-                    
-        elif isinstance(other,str):
-            temp=obj(dim=mat.dim,data=[[true if m[j][i]>other else false for i in range(d1)] for j in range(d0)],implicit=True)
+
+            data = []
+            for i in range(d0):
+                mrow= m[i]
+                data.append([true if mrow[j]>other else false for j in range(d1)])
             
         else:
             raise TypeError("Invalid type to compare")
+        
+        return obj(dim=mat.dim,
+                   data=data,
+                   features=mat.features[:],
+                   index=mat.index[:],
+                   implicit=True,BOOL_MAT=True,DEFAULT_BOOL={True:true,False:false})
 
-        return temp
-    
     else:
         pass
