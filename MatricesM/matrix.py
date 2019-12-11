1c9198e02d6a51873d8a859f3fd3baea2346daad
semihM <smhmmc98@gmail.com>
2019-12-11 08:55:13 +0300

    Modularize eigenvalues/vectors and SVD

--- MatricesM/matrix.py
+++ MatricesM/matrix.py
@@ -465,7 +465,7 @@ class Matrix(Vector):
     class level:
         def __init__(self,mat):
             if not mat._dfMat:
-                raise TypeError("Can't use row label indexing with non-dataframe matrices") 
+                raise TypeError("Can't use label indexing with non-dataframe matrices") 
             self.mat = mat
 
         def __getitem__(self,pos):
@@ -1055,145 +1055,15 @@ class Matrix(Vector):
         """
         Returns the eigenvalues using QR algorithm
         """
-        try:
-            assert self.isSquare and self.d0>=2
-            if self.d0==2:
-                d=self.det
-                tr=self.matrix[0][0]+self.matrix[1][1]
-                return list(set([(tr+(tr**2 - 4*d)**(1/2))/2,(tr-(tr**2 - 4*d)**(1/2))/2]))
-        
-            eigens = []
-            q=self.Q
-            a1=q.t@self@q
-            for i in range(self.QR_ITERS):#Iterations start
-                qq=a1.Q
-                a1=qq.t@a1@qq
-            #Determine which values are real and which are complex eigenvalues
-            if self.isSymmetric:#Symmetrical matrices always have real eigenvalues
-                return a1.diags
-
-            #Wheter or not dimensions are odd
-            isOdd=(a1.d0%2)
-            precision = self.PRECISION
-            #Decide wheter or not to skip the bottom right 2x2 matrix
-            if a1._cMat: 
-                neighbor = a1[-1,-2]
-                if round(neighbor.real,precision)==0 and round(neighbor.imag,precision):
-                    eigens.append(a1[-1,-1])
-            else:
-                if round(a1[-1,-2],precision)==0:
-                    eigens.append(a1[-1,-1])
-
-            #Create rest of the eigenvalues from 2x2 matrices
-            ind=0
-            while ind<a1.d0-1:
-                mat = a1[ind:ind+2,ind:ind+2]
-                ind+=1+isOdd
-
-                #Decide wheter or not to skip the top right corner 2x2 matrix
-                done=0
-                if a1._cMat:
-                    if round(mat[1,0].real,precision)==0 and round(mat[1,0].imag,precision):
-                        eigens.append(mat[0,0])
-                        ind-=isOdd
-                        done=1
-
-                elif round(mat[1,0],precision)==0:
-                    eigens.append(mat[0,0])
-                    ind-=isOdd
-                    done=1
-
-                #2x2 matrices in the middle
-                if not done:
-                    ind+=1-isOdd
-                    r = mat.trace/2
-                    v = (mat.det - r**2)**(1/2)
-                    
-                    r = complex(complex(roundto(r.real,precision,True)),complex(roundto(r.imag,precision,True)))
-                    v = complex(complex(roundto(v.real,precision,True)),complex(roundto(v.imag,precision,True)))               
-                    
-                    c1 = complex(r,v)
-                    c2 = complex(r,v*(-1))
-                    
-                    if c1.imag==0:
-                        c1 = c1.real
-                    if c2.imag==0:
-                        c2 = c2.real
-                    
-                    eigens.append(c1)
-                    eigens.append(c2)
-        except:
-            return None
-        else:
-            return eigens
+        from .linalg.eigen import _eigenvals
+        return _eigenvals(self,roundto)
 
     def _eigenvecs(self,iters,alpha=1+1e-5):
         """
         Returns the eigenvectors, eigenvector matrix and diagonal matrix
         """
-        eigens = self.eigenvalues or self.diags
-        if eigens in [None,[]]:
-            return None
-        
-        d0,d1 = self.dim[:]
-        ones = Matrix((self.d0,1),fill=1)
-        vectors = []
-        
-        for eig in eigens:
-            i = 0
-            c = None
-            x = ones.copy
-            eigen = eig*alpha
-            identity = Identity(d0)*(eigen)
-            
-            while i<iters:
-                try:
-                    y = ((self - identity).inv)@x
-                except:#Guess converged
-                    break
-                else:
-                    c = (y.t@x).matrix[0][0]/(x.t@x).matrix[0][0]
-                    if c == 0:
-                        c = None
-
-                    m = (y**2).sum("col_1",get=0)**(0.5)
-                    if m == 0:
-                        break
-
-                    x = y/m
-                    i += 1
-
-            guess = (1/c)+eig if c != None else eig
-            vectors.append((f"{i} iters for {eig}",guess,x))
-
-        ########################################################
-        eigenmat = vectors[0][2].copy
-        dtype_changed = False
-
-        for i in range(1,len(vectors)):
-            colvec = vectors[i][2]
-
-            if colvec.dtype == complex and not dtype_changed:
-                eigenmat.dtype = complex
-                dtype_changed == True
-
-            eigenmat.concat(colvec,axis=1)
-
-        ########################################################
-        diagmat = Matrix(len(vectors),fill=0,dtype=float)
-        dtype_changed = False
-
-        for i in range(len(vectors)):
-            eigvalue = vectors[i][1]
-            if isinstance(eigvalue,complex) and not dtype_changed:
-                diagmat.dtype = complex
-                dtype_changed = True
-
-            diagmat._matrix[i][i] = eigvalue
-
-        ########################################################
-
-        return (vectors,eigenmat,diagmat)
+        from .linalg.eigen import _eigenvecs
+        return _eigenvecs(self,iters,alpha,Identity,Matrix)
 
 # =============================================================================
     """Decomposition methods"""
@@ -1239,28 +1109,8 @@ class Matrix(Vector):
         """
         Singular value decomposition, Matrix = U@E@V.ht
         """
-        try:
-            transposed = self.t
-            
-            #self.t@self@V = V@E**2 --> solve eigenvalue problem
-            left_hand_side = transposed@self
-            E_and_V = left_hand_side.EIGENDEC
-            E = E_and_V[1]**(0.5) #square root of diagonal matrix
-            V = E_and_V[0].ht #hermitian transpose of the eigenvector matrix
-            
-            #self@self.t@U = U@E**2 --> solve eigenvalue problem
-            left_hand_side = self@transposed
-            U = left_hand_side.eigenvecmat
-
-            for diagonal in E.diags:
-                if isinstance(diagonal,complex):
-                    E.dtype = complex
-                    break
-                    
-        except:
-            return (None,None,None)
-        else:
-            return (U,E,V)
+        from .linalg.SVD import _SVD
+        return _SVD(self)
 
     def _hessenberg(self):
         pass
